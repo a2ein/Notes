6a380b89af6bacc82ba6d52882a15ac83a6f1547
a2ein <111061082+a2ein@users.noreply.github.com>
2023-12-27 12:49:14 +0600

    upload to

--- functionality/menu.js
+++ functionality/menu.js
@@ -1,9 +1,19 @@
-function src(first, second, third) {
-    let firstElement = document.getElementById('li1');
-    let secondElement = document.getElementById('li2');
-    let thirdElement = document.getElementById('li3');
+function src(first , second , third) {
 
-    firstElement.href = first;
-    secondElement.href = second;
-    thirdElement.href = third;
-  }
+
+let firstElement = document.getElementById('li1');
+
+let secondElement = document.getElementById('li2');
+
+let thirdElement = document.getElementById('li3');
+
+let a = document.getElementsByName('dis');
+firstElement.href=first;
+secondElement.href=second;
+thirdElement.href=third;
+for  (let a = 0; a < a.length; index++) {
+  const element = array[index];
+  
+}
+}src();
+
